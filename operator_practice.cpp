84c9db670a4b9140e1fc687f77ca77d51f83ead1
윤규민 <dbsrbals26@gmail.com>
2023-05-16 16:54:00 +0900

    cpp_operator_practice

--- operator_practice.cpp
+++ operator_practice.cpp
@@ -10,8 +10,7 @@ private:
 public:
     Point(int a = 0, int b = 0);
 
-    friend std::ostream &operator<<(std::ostream &out, const Point *ptr);
-
+    friend std::ostream &operator<<(std::ostream &out, const Point&ptr);
 
 };
 
@@ -20,15 +19,11 @@ Point::Point(int a, int b) {
     y = b;
 }
 
-std::ostream &operator<<(std::ostream &out, const Point *ptr) {
-    return (out << '[' << ptr->x << ',' << ptr->y << ']');
+std::ostream &operator<<(std::ostream &out, const Point &ptr) {
+    return (out << '[' << ptr.x << ',' << ptr.y << ']');
 }
 
-//이거 &를 사용했을때는 객체의 주솟값을 이용한다는 의미가 되지만 실상 이거는 매개변수에 넣어줄때는
-//객체 자체를 넣어줘야함 즉 주솟값을 넘겨주면안된다는소리임.
-//std::ostream &operator<<(std::ostream &out, const Point &ptr) {
-//    return (out << '[' << ptr.x << ',' << ptr.y << ']');
-//}
+
 
 int main() {
     Point *point = new Point(3, 5);
